9f631820c0dc2d43cb72a636d179df28643a65ba
Katie <crittykatie@gmail.com>
2024-09-29 21:54:57 -0600

    dynamic item display

--- flask/app.py
+++ flask/app.py
@@ -3,8 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    # Dummy data with quantity for each food item
+    food_items = [
+        {"name": "Food 1", "expiration": "2024-10-01", "quantity": 5},
+        {"name": "Food 2", "expiration": "2024-11-15", "quantity": 12},
+        {"name": "Food 3", "expiration": "2024-12-20", "quantity": 8}
+    ]
+    return render_template('index.html', food_items=food_items)
 
 if __name__ == '__main__':
     app.run(debug=True)
